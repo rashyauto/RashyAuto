1fab8a26fd10b9888e4cff8e2a362ae03674156b
rashyauto <113741914+rashyauto@users.noreply.github.com>
2022-09-17 03:10:51 +0100

    Add files via upload

---
+++ web.js
@@ -0,0 +1,13 @@
+alert("Welcome To rashy automobile");
+function myfuntion() {
+    document.getElementById("mymenu").classList.toggle("show")
+}
+
+window.onclick = funtionn(event) {
+    if
+    (event.target.matches('.menubtn')) {
+        var dropdowns = document.getElementsByClassName("menudown-content");
+    var i;
+    for (i = 0; i <dropdowns.length)     
+    }
+}
